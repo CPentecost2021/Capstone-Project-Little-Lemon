f5c3a17abbfdb5ae2649362d4d482d0869243705
Christopher Pentecost <cpentecost13@gmail.com>
2023-11-09 16:20:12 -0500

    Added new logo, and application files

---
+++ capstoneapp/src/Header.js
@@ -0,0 +1,12 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+  <header>
+  <img src="path/to/your/little-lemon-logo.png" alt="Little Lemon Logo" />
+</header>
+);
+}
+
+export default Header;
---
+++ capstoneapp/src/Main.js
@@ -0,0 +1,8 @@
+// Main.js
+import React from 'react';
+
+function Main() {
+  return <main>This is the main section</main>;
+}
+
+export default Main;
