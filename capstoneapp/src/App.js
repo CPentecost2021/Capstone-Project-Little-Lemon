ccb10dcfc61e4a591fca7e59e24edc1f42f7fe07
Christopher Pentecost <cpentecost13@gmail.com>
2023-11-27 19:52:29 -0500

    Added Browser Router to App.js

--- capstoneapp/src/App.js
+++ capstoneapp/src/App.js
@@ -4,9 +4,11 @@ import Nav from "./components/Nav"
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
     <React.Fragment>
       <Nav />
@@ -15,6 +17,7 @@ function App() {
       <Footer />
     </React.Fragment>
     </div>
+    </BrowserRouter>
   );
 }
 
